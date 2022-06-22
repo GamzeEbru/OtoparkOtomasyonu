d7a652b98d6dbf5f24f72f0ed214d36ad215fc2b
GamzeEbru <gamzeebrualtikulac@gmail.com>
2022-06-22 15:32:55 +0300

    commit

---
+++ otopark_otomasyonu/otopark_otomasyonu/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace otopark_otomasyonu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public SqlConnection bag = new SqlConnection("Data Source=DESKTOP-0QML4U0;Initial Catalog=otopark;Integrated Security=True");
+        public SqlCommand kmt = new SqlCommand();
+        public SqlDataAdapter adtr = new SqlDataAdapter();
+        public DataSet dtst = new DataSet();
+        public static string musteritc = null;
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlCommand komut = new SqlCommand("select * from musteri where TcKimlik='" + textBox1.Text + "' and sifre ='" + textBox2.Text + "'", bag);
+            
+            bag.Open();//bağlantıyı açık
+
+            SqlDataReader oku = komut.ExecuteReader();//veriyi okutma emrini verdik
+            if (oku.Read())//kullanıcı veritabanında kayıtlıysa
+            {
+                musteritc = textBox1.Text;
+                MessageBox.Show("Giriş Başarılı !");//giriş başarılı 
+                bag.Close();//bağlantıyı kapalı
+                musteri menu = new musteri();//yeni menü
+                menu.Show();//menü açar
+                this.Hide();//sayfayı gizler
+
+            }
+            else
+            {
+                bag.Close();//bağlantıyı kapalı
+                MessageBox.Show("Kullanıcı Adınız Yada Şifreniz Yanlış Yazılmıştır");//hayır veri okuyamadıysa uyarı verir
+                textBox1.Text = "";
+                textBox2.Text = "";
+                
+            }
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+           
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            sifremi_unuttum unuttum = new sifremi_unuttum();
+            unuttum.Show();
+            this.Hide();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlCommand komut = new SqlCommand("select * from personel where tc='" + textBox3.Text + "' and sifre ='" + textBox4.Text + "'", bag);
+         
+            bag.Open();
+
+            SqlDataReader oku = komut.ExecuteReader();
+            if (oku.Read())
+            {
+        
+                MessageBox.Show("Giriş Başarılı !");
+                bag.Close();//bağlantıyı kapalı
+                personel menu = new personel();
+                menu.Show();
+                this.Hide();////mevcut sayfayı gizler
+
+            }
+            else
+            {
+                bag.Close();
+                MessageBox.Show("Kullanıcı Adınız Yada Şifreniz Yanlış Yazılmıştır");
+                textBox3.Text = "";
+                textBox4.Text = "";
+              
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            SqlCommand komut = new SqlCommand("select * from admin where k_adi='" + textBox5.Text + "' and sifre ='" + textBox6.Text + "'", bag);
+           
+            bag.Open();
+
+            SqlDataReader oku = komut.ExecuteReader();//veriyi okutma emrini verdik
+            if (oku.Read())
+            {
+
+                MessageBox.Show("Giriş Başarılı !");
+                bag.Close();
+                teknik_servis menu = new teknik_servis();
+                menu.Show();
+                this.Hide();
+
+            }
+            else
+            {
+                bag.Close();
+                MessageBox.Show("Kullanıcı Adınız Yada Şifreniz Yanlış Yazılmıştır");
+                textBox5.Text = "";
+                textBox6.Text = "";
+        
+            }
+        }
+    }
+}
---
+++ otopark_otomasyonu/otopark_otomasyonu/musteri.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace otopark_otomasyonu
+{
+    public partial class musteri : Form
+    {
+        public musteri()
+        {
+            InitializeComponent();
+        }
+        public SqlConnection bag = new SqlConnection("Data Source=DESKTOP-0QML4U0;Initial Catalog=otopark;Integrated Security=True");
+        public SqlCommand kmt = new SqlCommand();
+        public SqlDataAdapter adtr = new SqlDataAdapter();
+        public DataSet dtst = new DataSet();
+        public void listelesene()
+        {
+            DataView dv = new DataView();
+            bag.Open();
+            SqlDataAdapter adtr = new SqlDataAdapter("select * From musteri", bag);
+            adtr.Fill(dtst, "musteri");
+
+            dv.Table = dtst.Tables[0];
+           
+            adtr.Dispose();
+            bag.Close();
+        }
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void musteri_Load(object sender, EventArgs e)
+        {
+            listelesene();
+            textBox1.DataBindings.Add("Text", dtst, "musteri.TcKimlik");
+            textBox2.DataBindings.Add("Text", dtst, "musteri.Ad");
+            textBox3.DataBindings.Add("Text", dtst, "musteri.Soyad");
+            textBox4.DataBindings.Add("Text", dtst, "musteri.CepTel");
+            textBox5.DataBindings.Add("Text", dtst, "musteri.PlakaNo");
+            textBox6.DataBindings.Add("Text", dtst, "musteri.Marka");
+            textBox7.DataBindings.Add("Text", dtst, "musteri.Model");
+            textBox8.DataBindings.Add("Text", dtst, "musteri.Renk");
+            textBox9.DataBindings.Add("Text", dtst, "musteri.Sifre");
+            textBox10.DataBindings.Add("Text", dtst, "musteri.Sifre");
+            comboBox1.DataBindings.Add("Text", dtst, "musteri.Gizli_Soru");
+            textBox11.DataBindings.Add("Text", dtst, "musteri.Yanit");
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                textBox10.PasswordChar = '\0';
+                textBox9.PasswordChar = '\0';
+            }
+            else
+            {
+                textBox10.PasswordChar = '*';
+                textBox9.PasswordChar = '*';
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlCommand guncelle = new SqlCommand("update musteri set TcKimlik='"+textBox1.Text+ "',Ad='"+textBox2.Text+ "',Soyad='"+textBox3.Text+ "',CepTel='"+textBox4.Text+ "',PlakaNo='"+textBox5.Text+ "',Marka='"+textBox6.Text+ "',Model='"+textBox7.Text+ "',Renk='"+textBox8.Text+ "',Sifre='"+textBox9.Text+ "',Gizli_Soru='"+comboBox1.Text+"',Yanit='"+textBox11.Text+ "' where TcKimlik='"+Form1.musteritc+"'", bag);
+            bag.Open();
+            guncelle.ExecuteNonQuery();
+            bag.Close();
+            MessageBox.Show("Güncelleme İşlemi Başarılı");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            SqlCommand komut = new SqlCommand("select * from musbil where TcKimlik='" + textBox1.Text + "' and PlakaNo ='" + textBox5.Text + "'", bag);
+            
+            bag.Open();
+
+            SqlDataReader oku = komut.ExecuteReader();
+            if (oku.Read())
+            {
+                MessageBox.Show("Aracınız Otoparktadır Konumu "+oku[9].ToString());
+                bag.Close();
+               
+            }
+            else
+            {
+                MessageBox.Show("Aracınız Otoparkta Bulunmamaktadır");
+                bag.Close();
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
